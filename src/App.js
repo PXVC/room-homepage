9a9e1f1f3fd19cd7fd20ffb1905a136cad6e030a
Pham Xuan Viet Cuong <pxvcuong2347@gmail.com>
2023-04-22 19:36:37 +0700

    Finish mobile UI

--- src/App.js
+++ src/App.js
@@ -24,6 +24,20 @@ function App() {
 					</a>
 				</div>
 			</section>
+			<section>
+				<img src={require("./assets/images/image-about-dark.jpg")} alt="about" />
+				<div className={classes.textBox}>
+					<h2>ABOUT OUR FURNITURE</h2>
+					<p>
+						Our multifunctional collection blends design and function to suit your
+						individual taste. Make each room unique, or pick a cohesive theme that best
+						express your interests and what inspires you. Find the furniture pieces you
+						need, from traditional to contemporary styles or anything in between. Product
+						specialists are available to help you create your dream space.
+					</p>
+				</div>
+				<img src={require("./assets/images/image-about-light.jpg")} alt="about" />
+			</section>
 		</div>
 	);
 }
